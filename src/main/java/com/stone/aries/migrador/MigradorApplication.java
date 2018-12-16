257c37d41c2ac12c091c7fedd601718a8d12e765
Anderson <anderson.gomes.c@gmail.com>
2018-12-16 13:06:27 -0300

    commit inicial do migrador

---
+++ src/main/java/com/stone/aries/migrador/MigradorApplication.java
@@ -0,0 +1,14 @@
+package com.stone.aries.migrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigradorApplication.class, args);
+	}
+
+}
+
